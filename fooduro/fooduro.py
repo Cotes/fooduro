e3d0ac3e3d031da08ae1f751e0c8b8c4b3b3e3f6
Cristian Cotes <cotes.cristian@gmail.com>
2017-04-03 00:14:38 +0200

    When food is consumed increase counter

---
+++ fooduro/fooduro.py
@@ -0,0 +1,4 @@
+class Fooduro:
+
+    def consume(self, food):
+        self.food = 1
--- tests/test_fooduro.py
+++ tests/test_fooduro.py
@@ -1,6 +1,12 @@
 import unittest
+from fooduro.fooduro import Fooduro
 
 class FooduroTest(unittest.TestCase):
 
     def test_is_working(self):
-        self.assertEqual(1, 1);
+        self.assertEqual(1, 1)
+
+    def test_when_food_is_consumed_counter_is_increased(self):
+        fooduro = Fooduro()
+        fooduro.consume("food")
+        self.assertEqual(1, fooduro.food)
