4aef7a006913e8e4f5d40c7f29fd6f37431df729
Cristian Cotes <cotes.cristian@gmail.com>
2017-04-05 00:24:09 +0200

    Refactor test

--- tests/test_fooduro.py
+++ tests/test_fooduro.py
@@ -5,12 +5,12 @@ from fooduro.fooduro import Fooduro
 class FooduroTest(unittest.TestCase):
 
     def setUp(self):
-        self.fooduro = Fooduro()
+        self.weekFood = Fooduro()
 
     def test_when_initialization_load_food(self):
-        self.fooduro.load("aDish", "anotherDish")
-        self.assertEqual(["aDish", "anotherDish"], self.fooduro.dishes)
+        self.weekFood.load("aDish", "anotherDish")
+        self.assertEqual(["aDish", "anotherDish"], self.weekFood.dishes)
 
     def test_when_we_ask_for_a_dish_should_serve_one(self):
-        self.fooduro.load("aDish")
-        self.assertIn(self.fooduro.whats_to_eat(), ["aDish"])
+        self.weekFood.load("aDish")
+        self.assertIn(self.weekFood.whats_to_eat(), ["aDish"])
