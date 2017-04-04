756d7b9810fcb15996b3a063c28f849956310049
Cristian Cotes <cotes.cristian@gmail.com>
2017-04-05 00:44:54 +0200

    Refactor test

--- tests/test_fooduro.py
+++ tests/test_fooduro.py
@@ -6,19 +6,17 @@ class FooduroTest(unittest.TestCase):
 
     def setUp(self):
         self.weekFood = WeekFood()
+        self.dishes = ["aDish", "anotherDish"]
+        self.weekFood.load(list(self.dishes))
 
     def test_when_initialization_load_food(self):
-        self.weekFood.load(["aDish", "anotherDish"])
         self.assertEqual(["aDish", "anotherDish"], self.weekFood.dishes)
 
     def test_when_we_ask_for_a_dish_should_serve_one(self):
-        self.weekFood.load(["aDish"])
         self.assertIn(self.weekFood.whats_to_eat(), ["aDish"])
 
     def test_asking_for_two_dishes_should_not_repeat_them_if_possible(self):
-        dishes = ["aDish", "anotherDish"]
-        self.weekFood.load(list(dishes))
         first_dish = self.weekFood.whats_to_eat()
-        self.assertIn(first_dish, dishes)
-        dishes.remove(first_dish)
-        self.assertIn(self.weekFood.whats_to_eat(), dishes)
+        self.assertIn(first_dish, self.dishes)
+        self.dishes.remove(first_dish)
+        self.assertIn(self.weekFood.whats_to_eat(), self.dishes)
